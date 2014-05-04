de6598518cc22e841a54c0ff933b30c36fe1d31f
originalblend337 <originalblend337@gmail.com>
2014-05-04 11:55:34 -0400

    added sessions controller and routes for user login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def index
+    @signin_link = User.oauth_link
+  end
+
+  def create
+    github_callback = User.parse_github_callback(params[:code])
+    client = User.new_github_client(github_callback["access_token"]).user
+    session[:github_access_token] = github_callback["access_token"]
+    unless User.exists?(username: client.login)
+      User.create(username: client.login, github_access_token: github_callback["access_token"])
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:github_access_token] = nil
+    redirect_to root_path
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+  def self.oauth_link
+    return "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT_ID']}"
+  end
+
+  def self.parse_github_callback(code)
+    return JSON.parse(RestClient.post("https://github.com/login/oauth/access_token", {client_id: ENV['CLIENT_ID'], client_secret: ENV['CLIENT_SECRET'], code: code}, { accept: :json }))
+  end
+
+  def self.new_github_client(access_token)
+    return Octokit::Client.new :access_token => access_token
+  end
+
 end
